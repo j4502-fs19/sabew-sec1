35d018ad50f4824bf51c270d7a010d6e00de841e
Allison Fu <allison@AllisondeMacBook-Pro.local>
2019-10-28 10:38:45 -0500

    js-menu

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 $(document).ready(function() {
   $('.menu-trigger').click(function() {
-    $('nav ul').slideToggle(500);
+    $('nav ul').slideToggle(600);
   });//end slide toggle
 
 //When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
   $(window).resize(function() {
-	   if (  $(window).width() > 700 ) {
+	   if (  $(window).width() > 800 ) {
 		$('nav ul').removeAttr('style');
 	 }
 	});//end resize
